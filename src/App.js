21e5c844753c9775b9d39220dc20f5caadde3bd4
itzmejus <itzjustin3344@gmail.com>
2021-11-05 04:58:47 -0700

    edited app.js

--- src/App.js
+++ src/App.js
@@ -10,12 +10,9 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
         >
-          Learn React
+          Welcome to my first React App
         </a>
       </header>
     </div>
