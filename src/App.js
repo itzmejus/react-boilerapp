de4d6c98ff932eb64cd5d2249146d0d9bc32b58d
itzmejus <itzjustin3344@gmail.com>
2021-11-05 05:07:10 -0700

    removed unwanted files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-
-        >
-          Welcome to my first React App
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
